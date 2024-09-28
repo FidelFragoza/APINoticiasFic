c79bf2b80f232e9e43ec6a7ff80d9db47def02fe
FidelFragoza <120699524+FidelFragoza@users.noreply.github.com>
2024-09-27 17:43:10 -0700

    Proyecto  - JSON

---
+++ APINoticiasFic/Controllers/NoticiasController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using APINoticiasFic.Models;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace APINoticiasFic.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoticiasController : ControllerBase
+    {
+        // GET: api/<NoticiasController>
+        [HttpGet]
+        public IEnumerable<Noticia> Get()
+        {
+            return Noticia.ObtenerNoticias();
+        }
+
+
+        // GET api/<NoticiasController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(string id)
+        {
+            Noticia encontrado = Noticia.ObtenerNoticia(id);
+            if (encontrado == null)
+            {
+                return NotFound(null);
+            }
+            return Ok(encontrado);
+        }
+
+
+        // POST api/<NoticiasController>
+        [HttpPost]
+        public void Post([FromBody] Noticia value)
+        {
+            Noticia.AgregarNoticia(value);
+        }
+
+        // POST api/<NoticiasController>/Autorizar/5
+        [HttpPost]
+        [Route("Autorizar/{id}")]
+        public IActionResult PostAutorizar(string id)
+        {
+            bool resultado = Noticia.AutorizarNoticia(id);
+            if (!resultado)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
+
+        // PUT api/<NoticiasController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(string id, [FromBody] Noticia value)
+        {
+            bool resultado = Noticia.ActualizarNoticia(id, value);
+            if (!resultado)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
+
+        // DELETE api/<NoticiasController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            bool resultado = Noticia.EliminarNoticia(id);
+            if (!resultado)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
+    }
+}
---
+++ APINoticiasFic/Models/Noticia.cs
@@ -0,0 +1,92 @@
+﻿namespace APINoticiasFic.Models
+{
+    public class Noticia
+    {
+            public string Id { get; set; }
+            public string Titulo { get; set; }
+            public string Contenido { get; set; }
+            public string Seccion { get; set; }
+            public string Autores { get; set; }
+            public DateTime Fecha { get; set; }
+            public int CantidadCaracteres { get; set; }
+            public bool Autorizada { get; set; }
+
+            public static IEnumerable<Noticia> ObtenerNoticias()
+            {
+                string fileName = @".\Models\Noticias.json";
+                string jsonString = System.IO.File.ReadAllText(fileName);
+                List<Noticia> listaNoticias = System.Text.Json.JsonSerializer.Deserialize<List<Noticia>>(jsonString).ToList();
+                return listaNoticias;
+            }
+
+            public static Noticia ObtenerNoticia(string id)
+            {
+                IEnumerable<Noticia> listaNoticias = ObtenerNoticias();
+                Noticia encontrado = listaNoticias.Where(x => x.Id == id).SingleOrDefault();
+                return encontrado;
+            }
+
+            public static void AgregarNoticia(Noticia value)
+            {
+                string fileName = @".\Models\Noticias.json";
+                List<Noticia> listaNoticias = ObtenerNoticias().ToList();
+                listaNoticias.Add(value);
+                string jsonString = System.Text.Json.JsonSerializer.Serialize(listaNoticias);
+                System.IO.File.WriteAllText(fileName, jsonString);
+            }
+
+            public static bool ActualizarNoticia(string id, Noticia value)
+            {
+                bool resultado = false;
+                string fileName = @".\Models\Noticias.json";
+                List<Noticia> listaNoticias = ObtenerNoticias().ToList();
+                Noticia encontrado = listaNoticias.Where(x => x.Id == id).SingleOrDefault();
+                if (encontrado != null)
+                {
+                    encontrado.Titulo = value.Titulo;
+                    encontrado.Contenido = value.Contenido;
+                    encontrado.Seccion = value.Seccion;
+                    encontrado.Autores = value.Autores;
+                    encontrado.Fecha = value.Fecha;
+                    encontrado.CantidadCaracteres = value.Contenido.Length;
+                    encontrado.Autorizada = value.Autorizada;
+                    string jsonString = System.Text.Json.JsonSerializer.Serialize(listaNoticias);
+                    System.IO.File.WriteAllText(fileName, jsonString);
+                    resultado = true;
+                }
+                return resultado;
+            }
+
+            public static bool EliminarNoticia(string id)
+            {
+                bool resultado = false;
+                string fileName = @".\Models\Noticias.json";
+                List<Noticia> listaNoticias = ObtenerNoticias().ToList();
+                Noticia encontrado = listaNoticias.Where(x => x.Id == id).SingleOrDefault();
+                if (encontrado != null)
+                {
+                    listaNoticias.Remove(encontrado);
+                    string jsonString = System.Text.Json.JsonSerializer.Serialize(listaNoticias);
+                    System.IO.File.WriteAllText(fileName, jsonString);
+                    resultado = true;
+                }
+                return resultado;
+            }
+
+            public static bool AutorizarNoticia(string id)
+            {
+                bool resultado = false;
+                string fileName = @".\Models\Noticias.json";
+                List<Noticia> listaNoticias = ObtenerNoticias().ToList();
+                Noticia encontrado = listaNoticias.Where(x => x.Id == id).SingleOrDefault();
+                if (encontrado != null)
+                {
+                    encontrado.Autorizada = true;
+                    string jsonString = System.Text.Json.JsonSerializer.Serialize(listaNoticias);
+                    System.IO.File.WriteAllText(fileName, jsonString);
+                    resultado = true;
+                }
+                return resultado;
+            }
+    }
+}
